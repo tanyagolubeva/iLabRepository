d67c63c5ee48e355933d4b2e19a34ec5a3396fd5
tanyagolubeva <golbeva-tatjana@rambler.ru>
2015-10-02 17:27:25 +0300

    first

---
+++ intel2/main.c
@@ -0,0 +1,16 @@
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+
+   char string [10];
+   scanf("%s", &string);
+
+   printf ("%d", strlen (string) );
+
+   return 0;
+}
---
+++ intel4/main2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compare (char* string1, char* string2);
+
+int main()
+{
+    char *s;
+    s = (char*)malloc(1024);
+    gets (s);
+    char *q;
+    q = (char*)malloc(1024);
+    gets (q);
+    int result = compare (s, q);
+    printf ("%d", result);
+    free (s);
+    free (q);
+    return 0;
+}
+
+int compare (char* string1, char* string2)
+{
+    int size = strlen (string1);
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        if (string1[i] != string2[i]) return 0;
+
+    }
+    return 1;
+}
