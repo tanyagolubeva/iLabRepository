3cee153d12a4116e802ba0b44a7a689aeacaac7c
tanyagolubeva <golbeva-tatjana@rambler.ru>
2015-10-06 23:18:34 +0300

    new

--- intel4/main2.c
+++ intel4/main2.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <assert.h>
 int compare (char* string1, char* string2);
+
 
 int main()
 {
@@ -20,13 +21,14 @@ int main()
 }
 
 int compare (char* string1, char* string2)
-{
-    int size = strlen (string1);
+{assert( string1 != NULL || string2 != NULL);{
+    int size = strlen(string1);
     int i;
     for (i = 0; i < size; i++)
     {
         if (string1[i] != string2[i]) return 0;
 
     }
+}
     return 1;
 }
